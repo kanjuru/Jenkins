b0980ab1aeb7aef41a6d00e6fd8890d2db4078c2
kanjuru <89851440+kanjuru@users.noreply.github.com>
2021-12-26 12:57:21 -0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 class Hello{
 	public static void main(String[] args) {
 		System.out.println("This is Java in collaboration with Jenkins!!!");
-		System.out.println("This is a webhook test trigger!");
+		System.out.println("This is a webhook test trigger!!");
 	}
 }
